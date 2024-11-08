48142170f2dd9b6a09bda3a9f2ee6e39f3dc37b8
LarissaNeitzke <larissasouza.neitzke@gmail.com>
2024-11-08 19:35:00 -0300

    FilmeCorraFísico.sql

--- 01-11/FilmeCorraFísico.sql
+++ 01-11/FilmeCorraFísico.sql
@@ -1,48 +1,48 @@
-/* FilmeCorraLógico: */
+/* Corra_Logico: */
 
 CREATE TABLE Personagem (
-    Personagem_ID INT PRIMARY KEY,
-    Etnia VARCHAR(252),
-    Genero VARCHAR(252),
-    Personagem_Nome VARCHAR(252),
-    fk_Tratamento_Tratamento_ID INT
+    Personagem_ID INTEGER PRIMARY KEY,
+    Personagem_Nome VARCHAR(30) NOT NULL,
+    Etnia VARCHAR(30) NOT NULL,
+    Genero VARCHAR(30) NOT NULL,
+    fk_Tratamento_Tratamento_ID INTEGER
 );
 
 CREATE TABLE Familia (
-    Familia_ID INT PRIMARY KEY,
-    Familia_Nome VARCHAR(252)
+    Familia_ID INTEGER PRIMARY KEY,
+    Familia_Nome VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE Contexto_Sociocultural (
-    Origem VARCHAR(252),
-    Classe_Social VARCHAR(252),
-    Crenca VARCHAR(252),
-    fk_Familia_Familia_ID INT,
-    fk_Personagem_Personagem_ID INT
+CREATE TABLE contexto_sociocultural (
+    Origem VARCHAR(30),
+    Classe_Social VARCHAR(30),
+    Crenca VARCHAR(30),
+    fk_Familia_Familia_ID INTEGER,
+    fk_Personagem_Personagem_ID INTEGER
 );
 
 CREATE TABLE Tratamento (
-    Tratamento_ID INT PRIMARY KEY,
-    Tratamento_Nome VARCHAR(252),
-    Descricao VARCHAR(252)
+    Tratamento_ID INTEGER PRIMARY KEY,
+    Tratamento_Nome VARCHAR(30) NOT NULL,
+    Descricao VARCHAR(200)
 );
 
 CREATE TABLE MembroFamilia (
-    Estado_Psicologico INT,
-    fk_Personagem_Personagem_ID INT,
-    fk_Familia_Familia_ID INT
+    Estado_Psicologico VARCHAR(30) NOT NULL,
+    fk_Personagem_Personagem_ID INTEGER,
+    fk_Familia_Familia_ID INTEGER
 );
  
 ALTER TABLE Personagem ADD CONSTRAINT FK_Personagem_2
     FOREIGN KEY (fk_Tratamento_Tratamento_ID)
     REFERENCES Tratamento (Tratamento_ID)
-    ON DELETE CASCADE;
+    ON DELETE RESTRICT;
  
-ALTER TABLE Contexto_Sociocultural ADD CONSTRAINT FK_Contexto_Sociocultural_1
+ALTER TABLE contexto_sociocultural ADD CONSTRAINT FK_contexto_sociocultural_1
     FOREIGN KEY (fk_Familia_Familia_ID)
     REFERENCES Familia (Familia_ID);
  
-ALTER TABLE Contexto_Sociocultural ADD CONSTRAINT FK_Contexto_Sociocultural_2
+ALTER TABLE contexto_sociocultural ADD CONSTRAINT FK_contexto_sociocultural_2
     FOREIGN KEY (fk_Personagem_Personagem_ID)
     REFERENCES Personagem (Personagem_ID);
  
